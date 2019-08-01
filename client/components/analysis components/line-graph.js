9fbfb5fe6eed842e98fe7ab32b9124162ad24df7
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-08-01 15:47:37 -0400

    Completed Data Visualization MVP

--- client/components/analysis components/line-graph.js
+++ client/components/analysis components/line-graph.js
@@ -1,19 +1,50 @@
 import React from 'react'
 import {Line, Tooltip} from 'britecharts-react'
 
+const ToolTipChildDataParser = prevData => {
+  return {
+    data: prevData.data.map(elem => ({
+      name: elem.name,
+      date: elem.date,
+      value: elem.value
+    }))
+  }
+}
+
 export const LineChart = props => {
+  // const reformattedData = ToolTipChildDataParser(props.data)
+  // console.log(reformattedData)
   return (
     <Line
+      // data={reformattedData}
       data={props.data}
-      lineCurve="basis"
+      // lineCurve="basis"
       margin={{top: 50, bottom: 50, left: 50, right: 50}}
       width="700"
+      shouldShowLoadingState={!props.data}
       // colorSchema=
-      // isAnimated="true"
+      // isAnimated={!props.data}
       {...props}
     />
   )
 }
+
+// export const LineChart = ({data}) => {
+//   // const reformattedData = ToolTipChildDataParser(props.data)
+//   // console.log(reformattedData)
+//   return (
+//     <Line
+//       data={data}
+//       // lineCurve="basis"
+//       margin={{top: 50, bottom: 50, left: 50, right: 50}}
+//       width="700"
+//       shouldShowLoadingState={!data}
+//       // colorSchema=
+//       // isAnimated={!props.data}
+//       // {...props}
+//     />
+//   )
+// }
 
 export const PersonalProgressChartWithToolTip = ({data}) => {
   return (
@@ -28,23 +59,44 @@ export const PersonalProgressChartWithToolTip = ({data}) => {
 
 export const PersonalProgressDataParser = climbingSessions => {
   const csFormatted = climbingSessions.map(session => {
-    const difficulties = session.climbs
-      .map(climb => climb.difficulty)
-      .join(', ')
     const score = session.climbs.reduce((accum, climb) => {
       return accum + climb.score
     }, 0)
     return {
-      topicName: difficulties,
-      name: 1,
       date: session.date,
       value: score
     }
   })
   return {
-    data: csFormatted
+    dataByTopic: [
+      {
+        topicName: 'Climbing',
+        topic: 'Climbing Power',
+        dates: csFormatted
+      }
+    ]
   }
 }
+
+// export const PersonalProgressDataParser = climbingSessions => {
+//   const csFormatted = climbingSessions.map((session) => {
+//     const difficulties = session.climbs
+//       .map(climb => climb.difficulty)
+//       .join(', ')
+//     const score = session.climbs.reduce((accum, climb) => {
+//       return accum + climb.score
+//     }, 0)
+//     return {
+//       topicName: difficulties,
+//       name: 1,
+//       date: session.date,
+//       value: score
+//     }
+//   })
+//   return {
+//     data: csFormatted
+//   }
+// }
 
 // {
 //   topicName: 'San Francisco',
--- client/components/analysis components/personal-analysis.js
+++ client/components/analysis components/personal-analysis.js
@@ -5,38 +5,11 @@ import {
 } from '../analysis components/line-graph'
 import {SampleUtility} from '../../store/sample'
 
-const testData = {
-  data: [
-    {
-      topicName: 'San Francisco',
-      name: 1,
-      date: '2017-01-16T16:00:00-08:00',
-      value: 1
-    },
-    {
-      topicName: 'San Francisco',
-      name: 1,
-      date: '2017-01-17T16:00:00-08:00',
-      value: 2
-    },
-    {
-      topicName: 'Oakland',
-      name: 2,
-      date: '2017-01-16T16:00:00-08:00',
-      value: 3
-    },
-    {
-      topicName: 'Oakland',
-      name: 2,
-      date: '2017-01-17T16:00:00-08:00',
-      value: 7
-    }
-  ]
-}
-
 export default class PersonalAnalysis extends Component {
   render() {
-    console.log(SampleUtility.getUserClimbingHistory(this.props.userId))
-    return <PersonalProgressChartWithToolTip data={testData} />
+    const data = PersonalProgressDataParser(
+      SampleUtility.getUserClimbingHistory(this.props.userId)
+    )
+    return <PersonalProgressChartWithToolTip data={data} />
   }
 }
