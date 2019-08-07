889ff5c67d2dad7197f639ce964e06ec3003b431
Xavier Olivares <xm.olivares@gmail.com>
2019-08-07 15:47:56 -0400

    chore(models): added models from mongodb

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,70 +1,28 @@
-const crypto = require('crypto')
-const Sequelize = require('sequelize')
-const db = require('../db')
+//to make modele you need to create a schema
 
-const User = db.define('user', {
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
   email: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false
+    type: String,
+    required: true,
+    unique: true
   },
   password: {
-    type: Sequelize.STRING,
-    // Making `.password` act like a func hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
-    get() {
-      return () => this.getDataValue('password')
-    }
+    type: String,
+    required: true
   },
-  salt: {
-    type: Sequelize.STRING,
-    // Making `.salt` act like a function hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
-    get() {
-      return () => this.getDataValue('salt')
-    }
+  avatar: {
+    type: String
   },
-  googleId: {
-    type: Sequelize.STRING
+  date: {
+    type: Date,
+    default: Date.now
   }
 })
 
-module.exports = User
-
-/**
- * instanceMethods
- */
-User.prototype.correctPassword = function(candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
-}
-
-/**
- * classMethods
- */
-User.generateSalt = function() {
-  return crypto.randomBytes(16).toString('base64')
-}
-
-User.encryptPassword = function(plainText, salt) {
-  return crypto
-    .createHash('RSA-SHA256')
-    .update(plainText)
-    .update(salt)
-    .digest('hex')
-}
-
-/**
- * hooks
- */
-const setSaltAndPassword = user => {
-  if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password(), user.salt())
-  }
-}
-
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
-User.beforeBulkCreate(users => {
-  users.forEach(setSaltAndPassword)
-})
+module.exports = User = mongoose.model('user', UserSchema)
